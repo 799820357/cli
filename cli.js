ec1910f17de21fa348d9af9e05b377729bd0775c
lichen <lichen105519@pwrd.com>
2022-08-09 10:22:29 +0800

    测试更新

--- cli.js
+++ cli.js
@@ -1,7 +1,7 @@
 const program = require('commander');
 const pkg = require('./package.json');
 const createReact = require('./command/create-react');
-//program
+//program-11
 program.version(pkg.version,'-v, --vers', 'output the current version');
 createReact(program);
 program.parse(process.argv);
